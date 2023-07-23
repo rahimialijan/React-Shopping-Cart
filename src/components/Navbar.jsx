1dc9845055eba03d8f12e320ba3e749ad7cc12f9
Alijan Rahimi <alijanrahimi24@yahoo.com>
2023-07-23 16:29:49 +0500

    Add pop-up for the webpage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,42 @@
-import { Navbar as NavbarBS, Button } from "react-bootstrap";
+import { useState } from "react";
+import { Navbar as NavbarBS, Button, Modal } from "react-bootstrap";
 import { BsCart } from "react-icons/bs";
 
 const Navbar = () => {
+  const [showModal, setShowModal] = useState(false);
+  const handleShowModal = () => {
+    setShowModal(true);
+  };
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
   return (
-    <NavbarBS className="border-bottom border-secondary">
-      <NavbarBS.Collapse className="justify-content-end">
-        <Button variant="btn btn-outline-secondary" className="text-white">
-          <BsCart className="mx-2" />
-          سبد خرید
-        </Button>
-      </NavbarBS.Collapse>
-    </NavbarBS>
+    <>
+      <NavbarBS className="border-bottom border-secondary">
+        <NavbarBS.Collapse className="justify-content-end">
+          <Button
+            onClick={handleShowModal}
+            variant="btn btn-outline-secondary"
+            className="text-white"
+          >
+            <BsCart className="mx-2" />
+            سبد خرید
+          </Button>
+        </NavbarBS.Collapse>
+      </NavbarBS>
+      <Modal
+        contentClassName="card-bg"
+        dir="rtl"
+        show={showModal}
+        onHide={handleCloseModal}
+      >
+        <Modal.Header closeButton closeVariant="white">
+          <Modal.Title>سبد خرید</Modal.Title>
+          <Modal.Body>محصول</Modal.Body>
+        </Modal.Header>
+      </Modal>
+    </>
   );
 };
 
